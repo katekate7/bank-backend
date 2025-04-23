ac3e12f97cedc942a6887d3937758db09d93edc2
katekate77 <katekr500@gmail.com>
2025-04-23 15:05:19 +0200

    Backend works

--- src/Controller/UserExpenseController.php
+++ src/Controller/UserExpenseController.php
@@ -17,45 +17,47 @@ class UserExpenseController extends AbstractController
     #[Route('/', name: 'app_user_expense_index', methods: ['GET'])]
     public function index(ExpenseRepository $expenseRepository): Response
     {
-        // Показуємо тільки свої витрати
-        $expenses = $expenseRepository->findBy(['user' => $this->getUser()]);
-
+        $user = $this->getUser();
+    
         return $this->render('user_expense/index.html.twig', [
-            'expenses' => $expenses,
+            'expenses' => $expenseRepository->findBy(['user' => $user]),
         ]);
     }
+    
 
     #[Route('/new', name: 'app_user_expense_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
         $expense = new Expense();
 
-        $expense->setUser($this->getUser());
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException('Користувач не залогінений');
+        }
+
+        $expense->setUser($user);
 
         $form = $this->createForm(ExpenseType::class, $expense);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($expense);
-            $entityManager->flush();
+            $em->persist($expense);
+            $em->flush();
 
-            return $this->redirectToRoute('app_user_expense_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_user_expense_index');
         }
 
-        return $this->renderForm('user_expense/new.html.twig', [
-            'expense' => $expense,
-            'form' => $form,
+        return $this->render('user_expense/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
+
+    
 
     #[Route('/{id}', name: 'app_user_expense_show', methods: ['GET'])]
     public function show(Expense $expense): Response
     {
-        // Перевірка доступу
-        if ($expense->getUser() !== $this->getUser()) {
-            throw $this->createAccessDeniedException();
-        }
-
         return $this->render('user_expense/show.html.twig', [
             'expense' => $expense,
         ]);
@@ -64,10 +66,6 @@ class UserExpenseController extends AbstractController
     #[Route('/{id}/edit', name: 'app_user_expense_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Expense $expense, EntityManagerInterface $entityManager): Response
     {
-        if ($expense->getUser() !== $this->getUser()) {
-            throw $this->createAccessDeniedException();
-        }
-
         $form = $this->createForm(ExpenseType::class, $expense);
         $form->handleRequest($request);
 
@@ -86,10 +84,6 @@ class UserExpenseController extends AbstractController
     #[Route('/{id}', name: 'app_user_expense_delete', methods: ['POST'])]
     public function delete(Request $request, Expense $expense, EntityManagerInterface $entityManager): Response
     {
-        if ($expense->getUser() !== $this->getUser()) {
-            throw $this->createAccessDeniedException();
-        }
-
         if ($this->isCsrfTokenValid('delete'.$expense->getId(), $request->request->get('_token'))) {
             $entityManager->remove($expense);
             $entityManager->flush();
